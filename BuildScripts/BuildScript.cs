b39ba19c25295c8f09ed4a93f3dee71520dd7206
igor <igor@localhost>
2016-01-11 13:44:33 +0100

    Switching to Nuget-based Flubu. Added test coverage in the build script. Updated other 3rd party nuget packages.

--- BuildScripts/BuildScript.cs
+++ BuildScripts/BuildScript.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using Flubu;
 using Flubu.Builds;
 using Flubu.Builds.Tasks.AnalysisTasks;
-using Flubu.Builds.VSSolutionBrowsing;
+using Flubu.Builds.Tasks.TestingTasks;
 using Flubu.Targeting;
-using Flubu.Tasks.Processes;
 
 //css_ref Flubu.dll;
 //css_ref Flubu.Contrib.dll;
-//css_ref log4net.dll;
-//css_ref ICSharpCode.SharpZipLib.dll;
 
 namespace BuildScripts
 {
@@ -17,76 +17,36 @@ namespace BuildScripts
     {
         public static int Main(string[] args)
         {
-            TargetTree targetTree = new TargetTree();
-            BuildTargets.FillBuildTargets(targetTree);
+            DefaultBuildScriptRunner runner = new DefaultBuildScriptRunner(ConstructTargets, ConfigureBuildProperties);
+            return runner.Run(args);
+        }
 
+        private static void ConstructTargets(TargetTree targetTree)
+        {
             targetTree.AddTarget("rebuild")
                 .SetDescription("Builds the product, packages it, deploys and tests it on a local instance")
-                .DependsOn("compile", "dupfinder", "tests")
-                .SetAsDefault();
+                .DependsOn("compile", "dupfinder", "tests").SetAsDefault();
 
-            targetTree.GetTarget ("fetch.build.version")
-                .Do (TargetFetchBuildVersion);
-
+            targetTree.GetTarget("fetch.build.version").Do(TargetFetchBuildVersion);
+            
             targetTree.AddTarget("tests")
-                .SetDescription("Runs tests on the project")
-                .Do (r =>
-                    {
-                        TargetRunTests(r, "Freude.Tests");
-                    }).DependsOn ("load.solution");
-
+                .SetDescription("Runs tests on the project").Do(TargetRunTests).DependsOn("load.solution");
+            
             targetTree.AddTarget("dupfinder")
-                .SetDescription("Runs R# dupfinder to find code duplicates")
-                .Do(TargetDupFinder);
+                .SetDescription("Runs R# dupfinder to find code duplicates").Do(TargetDupFinder);
+        }
 
-            using (TaskSession session = new TaskSession(new SimpleTaskContextProperties(), args, targetTree))
-            {
-                BuildTargets.FillDefaultProperties(session);
-                session.Start(BuildTargets.OnBuildFinished);
-
-                session.AddLogger(new MulticoloredConsoleLogger(Console.Out));
-
-                session.Properties.Set(BuildProps.CompanyName, "Igor Brejc");
-                session.Properties.Set(BuildProps.CompanyCopyright, "Copyright (C) 2015 Igor Brejc");
-                session.Properties.Set(BuildProps.ProductId, "Freude");
-                session.Properties.Set(BuildProps.ProductName, "Freude");
-                session.Properties.Set(BuildProps.SolutionFileName, "Freude.sln");
-                session.Properties.Set(BuildProps.TargetDotNetVersion, FlubuEnvironment.Net40VersionNumber);
-                session.Properties.Set(BuildProps.VersionControlSystem, VersionControlSystem.Mercurial);
-                session.Properties.Set(BuildProps.FxcopDir, "Microsoft Fxcop 10.0");
-
-                try
-                {
-                    // actual run
-                    if (args.Length == 0)
-                        targetTree.RunTarget(session, targetTree.DefaultTarget.TargetName);
-                    else
-                    {
-                        string targetName = args[0];
-                        if (false == targetTree.HasTarget(targetName))
-                        {
-                            session.WriteError("ERROR: The target '{0}' does not exist", targetName);
-                            targetTree.RunTarget(session, "help");
-                            return 2;
-                        }
-
-                        targetTree.RunTarget(session, args[0]);
-                    }
-
-                    session.Complete();
-
-                    return 0;
-                }
-                catch (TaskExecutionException)
-                {
-                    return 1;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    return 1;
-                }
-            }
+        private static void ConfigureBuildProperties(TaskSession session)
+        {
+            session.Properties.Set(BuildProps.CompanyName, "Igor Brejc");
+            session.Properties.Set(BuildProps.CompanyCopyright, "Copyright (C) 2015-2016 Igor Brejc");
+            session.Properties.Set(BuildProps.ProductId, "Freude");
+            session.Properties.Set(BuildProps.ProductName, "Freude");
+            session.Properties.Set(BuildProps.SolutionFileName, "Freude.sln");
+            session.Properties.Set(BuildProps.TargetDotNetVersion, FlubuEnvironment.Net40VersionNumber);
+            session.Properties.Set(BuildProps.ToolsVersion, FlubuEnvironment.Net40VersionNumber);
+            session.Properties.Set(BuildProps.VersionControlSystem, VersionControlSystem.Mercurial);
+            session.Properties.Set(BuildProps.FxcopDir, "Microsoft Fxcop 10.0");
         }
 
         private static void TargetFetchBuildVersion (ITaskContext context)
@@ -96,23 +56,19 @@ namespace BuildScripts
             context.WriteInfo ("The build version will be {0}", version);
         }
 
-        private static void TargetRunTests(ITaskContext context, string projectName)
+        private static void TargetRunTests(ITaskContext context)
         {
-            VSSolution solution = context.Properties.Get<VSSolution>(BuildProps.Solution);
-            string buildConfiguration = context.Properties.Get<string>(BuildProps.BuildConfiguration);
+            string config = context.Properties[BuildProps.BuildConfiguration];
+            List<string> testAssemblies = new List<string> ();
+            testAssemblies.Add (Path.Combine (string.Format(CultureInfo.InvariantCulture, "Freude.Tests/bin/{0}/Freude.Tests.dll", config)));
 
-            VSProjectWithFileInfo project =
-                (VSProjectWithFileInfo)solution.FindProjectByName(projectName);
-            FileFullPath projectTarget = project.ProjectDirectoryPath.CombineWith(project.GetProjectOutputPath(buildConfiguration))
-                .AddFileName("{0}.dll", project.ProjectName);
-
-            RunProgramTask task = new RunProgramTask(
-                @"packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe")
-                .AddArgument(projectTarget.ToString())
-                .AddArgument("/labels")
-                .AddArgument("/trace=Verbose")
-                .AddArgument("/nodots");
-            task.Execute(context);
+            NUnitWithDotCoverTask task = new NUnitWithDotCoverTask (
+                @"packages\NUnit.Console.3.0.1\tools\nunit3-console.exe",
+                testAssemblies);
+            task.FailBuildOnViolations = false;
+            task.NUnitCmdLineOptions = "--labels=All --trace=Verbose --verbose";
+            task.DotCoverFilters = "-:module=*.Tests;-:class=*Contract;-:class=*Contract`*;-:module=LibroLib*;-:module=Syborg;";
+            task.Execute (context);
         }
 
         private static void TargetDupFinder(ITaskContext context)
