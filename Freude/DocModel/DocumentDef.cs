88efd80eaf93688080311dcf429ed341dbfdd0d6
igor <igor@localhost>
2016-01-11 13:13:03 +0100

    Importing project source code from other repo.

---
+++ Freude/DocModel/DocumentDef.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Freude.DocModel
+{
+    public class DocumentDef : IDocumentElementContainer
+    {
+        public int ChildrenCount
+        {
+            get { return children.Count; }
+        }
+
+        public IList<IDocumentElement> Children
+        {
+            get { return children; }
+        }
+
+        public void AddChild(IDocumentElement child)
+        {
+            children.Add(child);
+        }
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(children != null);
+        }
+
+        private readonly List<IDocumentElement> children = new List<IDocumentElement>();
+    }
+}
